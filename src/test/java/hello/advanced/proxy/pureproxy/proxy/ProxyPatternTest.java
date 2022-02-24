335c21eedf01c14d7bdefb8913e356bc6a0060dc
hyeonse <dlgustp1487@naver.com>
2022-02-24 20:36:28 +0900

    프록시 패턴 - 예제 코드 1

---
+++ src/test/java/hello/advanced/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,17 @@
+package hello.advanced.proxy.pureproxy.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import hello.advanced.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.advanced.proxy.pureproxy.proxy.code.RealSubject;
+
+public class ProxyPatternTest {
+	@Test
+	void noProxyTest() {
+		RealSubject realSubject = new RealSubject();
+		ProxyPatternClient client = new ProxyPatternClient(realSubject);
+		client.execute();
+		client.execute();
+		client.execute();
+	}
+}
