e6ddcfb61c8c47c387bca4a142d5f89f5e183713
이현세 <dlgustp1487@naver.com>
2022-03-02 13:48:52 +0900

    스프링이 제공하는 빈 후처리기1

---
+++ src/main/java/hello/advanced/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,29 @@
+package hello.advanced.proxy.config.v5_autoproxy;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import hello.advanced.proxy.config.AppV1Config;
+import hello.advanced.proxy.config.AppV2Config;
+import hello.advanced.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.advanced.template.trace.logtrace.LogTrace;
+
+@Configuration
+@Import({ AppV1Config.class, AppV2Config.class })
+public class AutoProxyConfig {
+	
+	@Bean
+	public Advisor advisor1(LogTrace logTrace) {
+		// pointcut
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedNames("request*", "order*", "save*");
+		// advice
+		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+}
