7793d64ae4c15a2a339dc96332641e001fb342d6
이현세 <dlgustp1487@naver.com>
2022-03-02 15:32:11 +0900

    @Aspect 프록시 - 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -10,6 +10,7 @@ import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.advanced.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.advanced.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.advanced.proxy.config.v6_aop.AopConfig;
 import hello.advanced.template.trace.logtrace.LogTrace;
 import hello.advanced.template.trace.logtrace.ThreadLocalLogTrace;
 
@@ -21,7 +22,8 @@ import hello.advanced.template.trace.logtrace.ThreadLocalLogTrace;
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app")	//주의
 public class AdvancedApplication {
 
